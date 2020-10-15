b52b4f0be470ff0825faf0f4e1c7cb4babb86fd8
Mathias Göbel <goebel@sub.uni-goettingen.de>
2020-10-15 12:42:04 +0200

    add a test for dependencies in YAML.

---
+++ yaml-test.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# test and validate YAML files for DARIAH-DE status
+# requires YAML CLI parser from npm, do npm install -g yaml-cli
+
+# globals
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+BOLD=$(tput bold)
+NORM=$(tput sgr0)
+
+yaml >> /dev/null
+if [[ $? -gt 0 ]]; then echo "YAML parser not found."; exit; fi;
+
+for FILE in {_infrastructure,_servers,_middlewares,_services}/*.yaml; do
+  # prepare temporary copy of the files
+  cp $FILE $FILE.tmp;
+  # remove YAML header marker from files
+  sed "s \-\-\-  g" -i $FILE.tmp;
+done;
+
+# prepare item list
+LIST=$(ls **/*.tmp | sed -e "s _ / g" -e "s \.yaml\.tmp  g")
+# make it a bash array
+declare -a array=($LIST)
+echo "the list contains ${#array[@]} items"
+
+
+# work on the temporary files
+for FILE in {_infrastructure,_servers,_middlewares,_services}/*.tmp; do
+  NAME=$(sed -e "s _  g" -e "s \.yaml\.tmp  g" <<< $FILE)
+  DEP=($(yaml get $FILE dependencies));
+  echo "$NAME: found ${#DEP[@]} dependencies";
+  
+  for DEPi in ${DEP[*]}; do
+    [[ ${array[*]} =~ "$DEPi" ]] && true || echo -e "${RED}${BOLD}$(sed 's/./ /g' <<< $NAME): $DEPi nil.${NORM}${NC}"
+  done;
+  
+  rm $FILE;
+  
+done;
